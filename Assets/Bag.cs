ed54f6a8e753d3bcc6325313e2a5aab0743b775b
yjp900918 <yjp900918@gmail.com>
2018-04-01 11:49:28 -0400

    111

---
+++ Assets/Bag.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bag : MonoBehaviour {
+
+    public GameObject slot;
+    public GameObject item;
+
+    ItemDataBase itemdatabase;
+
+    public List<GameObject> slots = new List<GameObject>();
+    public List<Item> items = new List<Item>();
+
+    GameObject slotPanel;
+	// Use this for initialization
+	void Start () {
+        itemdatabase = GetComponent<ItemDataBase>();
+        slotPanel = GameObject.Find("Slot Panel");
+
+        for (int i = 0; i < 16; i++)
+        {
+            slots.Add(Instantiate(slot));
+            slots[i].transform.SetParent(slotPanel.transform);
+            items.Add(new Item());
+        }
+        AddItem(0);
+        AddItem(1);
+
+
+    }
+	
+    public void AddItem(int _id)
+    {
+        Item itemToAdd = itemdatabase.FetchItemByID(_id);
+        
+        for (int i = 0; i < items.Count; i++)
+        {
+            if(items[i].ID == -1)
+            {
+                items[i] = itemToAdd;
+                GameObject itemObj = Instantiate(item);
+                itemObj.transform.SetParent(slots[i].transform);
+                itemObj.transform.position = Vector2.zero;
+                itemObj.name = itemToAdd.Title;
+                itemObj.GetComponent<Image>().sprite = itemToAdd.sprite;
+                break;
+            }
+        }
+
+    }
+	
+}
---
+++ Assets/ItemDataBase.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+using System.IO;
+
+public class ItemDataBase : MonoBehaviour {
+    private JsonData itemdata;
+    private List<Item> database = new List<Item>();
+	// Use this for initialization
+	void Start () {
+        itemdata = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/ItemBase/items.json"));
+
+        ConstructItemDatabase();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ConstructItemDatabase()
+    {
+        for (int i = 0; i < itemdata.Count; i++)
+        {
+            database.Add(new Item((int)itemdata[i]["id"], itemdata[i]["title"].ToString(), (int)itemdata[i]["value"], itemdata[i]["description"].ToString(), itemdata[i]["slug"].ToString()));
+        }
+    }
+
+    public Item FetchItemByID(int _id)
+    {
+        for (int i = 0; i < database.Count; i++)
+        {
+            if(_id == database[i].ID)
+            {
+                return database[i];
+            }
+        }
+        return null;
+    }
+}
+
+public class Item
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public int Value { get; set; }
+    public string Description { get; set; }
+    public Sprite sprite { get; set; }
+
+    public Item(int _id, string _title, int _value, string _description, string _slug)
+    {
+        this.ID = _id;
+        this.Title = _title;
+        this.Value = _value;
+        this.Description = _description;
+        this.sprite = Resources.Load<Sprite>("Items/" + _slug);
+    }
+
+    public Item()
+    {
+        this.ID = -1;
+    }
+}
